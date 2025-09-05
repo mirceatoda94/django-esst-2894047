46400f099233f0d381e3f8b3ec49a9a691b08e3b
mirceatoda94 <147103758+mirceatoda94@users.noreply.github.com>
2025-09-05 13:41:37 +0000

    link based navigation and bootstrap styling.

--- notes/urls.py
+++ notes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("notes/", views.NoteListView.as_view(), name="all_notes"),
-    path("notes/<int:pk>/", views.NoteDetailView.as_view(), name="note_detail"),
-    path("notes/popular/", views.PopularNotesListView.as_view(), name="popular_notes"),
+    path("notes/", views.NoteListView.as_view(), name="notes.list"),
+    path("notes/<int:pk>/", views.NoteDetailView.as_view(), name="notes.detail"),
+    path("notes/popular/", views.PopularNotesListView.as_view(), name="notes.popular"),
 ]
